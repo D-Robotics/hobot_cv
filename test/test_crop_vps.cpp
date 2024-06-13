c8f8ba084e8bbfede2f811587ce5816d1f9b4d8a
zhikang.zeng <zhikang.zeng@horizon.cc>
2024-06-13 12:50:31 +0000

    refactor: update test case

--- test/test_crop_vps.cpp
+++ test/test_crop_vps.cpp
@@ -62,7 +62,7 @@ int main() {
     ss_crop << "crop image to " << dst_width << "x" << dst_height << " pixels" << ", mat input, time cost: " << interval << " ms";
     RCLCPP_INFO(rclcpp::get_logger("example"), "%s", ss_crop.str().c_str());
     // 保存结果
-    writeImg(cropmat, "./crop_mat_input_vps_process.jpg");
+    writeImg(cropmat, "./crop_mat_input_vps_process_1.jpg");
   }
 
   // 4.crop图像、测试耗时，输入图像为数组格式
@@ -83,7 +83,49 @@ int main() {
       RCLCPP_INFO(rclcpp::get_logger("example"), "%s", ss_crop.str().c_str());
       cv::Mat dst_mat(imageInfo->height * 3 / 2, imageInfo->width, CV_8UC1, imageInfo->imageAddr);
       // 保存结果
-      writeImg(dst_mat, "./crop_arr_input_vps_process.jpg");
+      writeImg(dst_mat, "./crop_arr_input_vps_process_1.jpg");
+    }
+  }
+
+  // 4.crop图像、测试耗时，输入图像为Mat格式，会缩放再截取
+  {
+    RCLCPP_INFO(rclcpp::get_logger("example"), "%s", "Test hobotcv_crop: Mat Input, VPS Process");
+    auto before_crop = std::chrono::system_clock::now();
+    // =============================================
+    auto cropmat = hobot_cv::hobotcv_crop(srcmat_nv12, src_height, src_width, dst_height, dst_width, cv::Range(0, dst_height+10), cv::Range(0, dst_width+10), hobot_cv::HOBOTCV_VPS);
+    // =============================================
+    auto after_crop = std::chrono::system_clock::now();
+    auto interval = std::chrono::duration_cast<std::chrono::milliseconds>(after_crop - before_crop).count();
+    std::stringstream ss;
+    ss << "source image " << image_file << " is " << src_width << "x" << src_height << " pixels";
+    RCLCPP_INFO(rclcpp::get_logger("example"), "%s", ss.str().c_str());
+
+    std::stringstream ss_crop;
+    ss_crop << "crop image to " << dst_width << "x" << dst_height << " pixels" << ", mat input, time cost: " << interval << " ms";
+    RCLCPP_INFO(rclcpp::get_logger("example"), "%s", ss_crop.str().c_str());
+    // 保存结果
+    writeImg(cropmat, "./crop_mat_input_vps_process_2.jpg");
+  }
+
+  // 4.crop图像、测试耗时，输入图像为数组格式，会缩放再截取
+  {
+    RCLCPP_INFO(rclcpp::get_logger("example"), "%s", "Test hobotcv_crop: Arr Input, VPS Process");
+    auto before_crop = std::chrono::system_clock::now();
+    // =============================================
+    auto imageInfo = hobot_cv::hobotcv_crop(reinterpret_cast<const char *>(srcmat_nv12.data), src_height, src_width, dst_height, dst_width, cv::Range(0, dst_height+10), cv::Range(0, dst_width+10), hobot_cv::HOBOTCV_VPS);
+    auto after_crop = std::chrono::system_clock::now();
+    // =============================================
+    auto interval = std::chrono::duration_cast<std::chrono::milliseconds>(after_crop - before_crop).count();
+    std::stringstream ss;
+    ss << "source image " << image_file << " is " << src_width << "x" << src_height << " pixels";
+    RCLCPP_INFO(rclcpp::get_logger("example"), "%s", ss.str().c_str());
+    if (imageInfo != nullptr) {
+      std::stringstream ss_crop;
+      ss_crop << "crop image to " << dst_width << "x"<< dst_height << " pixels"<< ", arr input, time cost: " << interval << " ms";
+      RCLCPP_INFO(rclcpp::get_logger("example"), "%s", ss_crop.str().c_str());
+      cv::Mat dst_mat(imageInfo->height * 3 / 2, imageInfo->width, CV_8UC1, imageInfo->imageAddr);
+      // 保存结果
+      writeImg(dst_mat, "./crop_arr_input_vps_process_2.jpg");
     }
   }
 
